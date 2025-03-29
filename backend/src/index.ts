33807fd15ef817ee6708cc562ff23ee8c7fab53e
om123s <awesomeomsurve@gmail.com>
2025-03-29 19:21:36 -0400

    Connect to MongoDB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
-const PORT = 5000;
+const PORT = 6969;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is live');
-});
+//connecting to database through connection string
+const dbURI = 'mongodb+srv://awesomeomsurve:4BXq1l6JPTLdxq4p@cluster2.vlpzm8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2';
+mongoose.connect(dbURI)
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log('MongoDB connection error:', err));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+  app.get('/', (req, res) => {
+  res.send('API is live');
 });
